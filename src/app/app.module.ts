2cc129774f214c546ae360754d1dd4a7dc0e7747
Zijian Huang <jessehuang05@yahoo.com>
2022-03-13 14:31:27 -0500

    Continue on router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { UserCenterComponent } from './user-center/user-center.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RouterModule } from '@angular/router';
+import { InfoComponent } from './info/info.component';
+import { AvatarComponent } from './avatar/avatar.component';
+import { SecurityComponent } from './security/security.component';
+const routes =[
+  {path: 'index',component:IndexComponent},
+  {path: '',component:IndexComponent},
+  {path:'user-center',component:UserCenterComponent,
+children:[
+  {path:'info',component:InfoComponent},
+  {path:'avatar',component:AvatarComponent},
+  {path:'security',component:SecurityComponent}
+]
+},
+  {path:'**',component:NotFoundComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    UserCenterComponent,
+    NotFoundComponent,
+    InfoComponent,
+    AvatarComponent,
+    SecurityComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot( routes )
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
